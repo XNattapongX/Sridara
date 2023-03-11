127835db889c6e4efaaf39b81b906c76b88a46ab
OB02CGTH <sornsiri1208@gmail.com>
2023-03-11 21:37:37 +0700

    Update Index

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,7 @@
   <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead
-          class="text-xs text-gray-700 uppercase bg-gray-50 dark:text-gray-400"
-          style="background-color: #81c784">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:text-gray-400" style="background-color: #81c784">
           <tr>
             <th scope="col" class="px-6 py-3">ชื่อทริปทัวร์</th>
             <th scope="col" class="px-6 py-3">ชื่อโปรแกรมทัวร์</th>
@@ -19,10 +17,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr
-            class="table-row-hover"
-            v-for="(item, index) in tour_ls"
-            :key="index">
+          <tr class="table-row-hover" v-for="(item, index) in tour_ls" :key="index">
             <td class="px-6 py-4">
               {{ item.fields.trip_name.stringValue }}
             </td>
@@ -58,6 +53,7 @@
 </template>
 
 <script>
+import { string } from "vue-types";
 import { read_all_data } from "~~/services/configs";
 export default {
   mounted() {
@@ -72,7 +68,13 @@ export default {
       tour_ls: [],
     };
   },
+  methods: {
+    detail_tour(id) {
+      this.$router.push({ name: '/tourdata', params: { id: id } })
+    },
+  }
 };
+
 </script>
 
 <style scoped>
