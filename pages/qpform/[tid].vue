3551934df55f35bc4829e1c763a08834f75d7d06
BesTTTTTTT <thanawatbest1234@gmail.com>
2023-03-13 15:49:13 +0700

    Best

--- pages/qpform/[tid].vue
+++ pages/qpform/[tid].vue
@@ -239,7 +239,10 @@
               v-model="product_tax"
               class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
           </v-col>
-          <v-col>
+        
+        </v-row>
+          <v-row>
+            <v-col>
             <label
               for="base-input"
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -251,6 +254,19 @@
               v-model="product_total"
               class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
           </v-col>
+          <v-col>
+            <label
+              for="base-input"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >กำหนดยืนราคากี่  วัน</label
+            >
+            <input
+              type="text"
+              id="small-input"
+              v-model="product_total"
+              class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            </v-col>
+           
           <v-col cols="2">
             <br />
             <v-btn
