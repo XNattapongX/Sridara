615cd57231e4ed23d97d0a8b774e609a99fe1251
Apiwan <gamebp02@hotmail.com>
2023-03-21 12:16:48 +0700

    ApiwanWorkShops

--- pages/index.vue
+++ pages/index.vue
@@ -12,8 +12,8 @@
             <th scope="col" class="px-6 py-3">ชื่อโปรแกรมทัวร์</th>
             <th scope="col" class="px-6 py-3">วันที่เริ่มทริป</th>
             <th scope="col" class="px-6 py-3">วันที่จบทริปงับ</th>
-            <th scope="col" class="px-6 py-3">วัน</th>
-            <th scope="col" class="px-6 py-3">คืน</th>
+            <th scope="col" class="px-6 py-3">วันที่อากาศสดใสไร้เมฆหมอก</th>
+            <th scope="col" class="px-6 py-3">คืนที่ดาวเต็มฟ้า</th>
             <th scope="col" class="px-6 py-3">เที่ยวบินหรือพาหนะอื่นขาไป</th>
             <th scope="col" class="px-6 py-3">เที่ยวบินหรือพาหนะอื่นขากลับ</th>
 
