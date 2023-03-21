c8241c2c95ff29c898eba0991c41b0a596670b16
Apiwan <gamebp02@hotmail.com>
2023-03-21 12:51:13 +0700

    ApiwanEdit

--- pages/index.vue
+++ pages/index.vue
@@ -8,24 +8,13 @@
           class="text-xs text-gray-700 uppercase bg-gray-50 dark:text-gray-400"
           style="background-color: #81c784">
           <tr>
-            <th scope="col" class="px-6 py-3">ชื่อทริปทัวร์</th>
-            <th scope="col" class="px-6 py-3">ชื่อโปรแกรมทัวร์</th>
-            <th scope="col" class="px-6 py-3">วันที่เริ่มทริป</th>
+            <th scope="col" class="px-6 py-3">ผมรักลุงตู่</th>
+            <th scope="col" class="px-6 py-3">คิดถึงคนบนฟ้า</th>
+            <th scope="col" class="px-6 py-3">ไม่ใช่สลิ่มแค่เป็นคนรักชาติ</th>
             <th scope="col" class="px-6 py-3">วันที่จบทริปงับ</th>
             <th scope="col" class="px-6 py-3">วันที่อากาศสดใสไร้เมฆหมอก</th>
             <th scope="col" class="px-6 py-3">คืนที่ดาวเต็มฟ้า</th>
-            <th scope="col" class="px-6 py-3">วัน</th>
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <th scope="col" class="px-6 py-3">คืนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนน</th>
->>>>>>> 0aa71e89e8e194d04610524a7af07028e5829f42
-=======
-            <th scope="col" class="px-6 py-3">คืนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนนwdadadasdadsdaasdadadadasdadasdadds</th>
->>>>>>> fc3980b52b5893581a799223777dd0959ae3fbda
-=======
-            <th scope="col" class="px-6 py-3">คืน</th>
             <th scope="col" class="px-6 py-3">เที่ยวบินหรือพาหนะอื่นขาไป</th>
-            <th scope="col" class="px-6 py-3">เที่ยวบินหรือพาหนะอื่นขาไปขามา</th>
             <th scope="col" class="px-6 py-3">เที่ยวบินหรือพาหนะอื่นขากลับ</th>
 
             <th scope="col" class="px-6 py-3">จำนวนลูกทัวร์</th>
